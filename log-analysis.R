56cc628caaa099ca8b9165fede099a176d90b92d
raibpn <raibpn@users.noreply.github.com>
2023-01-30 20:27:47 +0100

    salary and success rate correlation

--- log-analysis.R
+++ log-analysis.R
@@ -1,3 +1,6 @@
+install.packages("ggpubr")
+install.packages("dplyr")
+
 library(tidyverse)
 library(dplyr)
 library(readr)
@@ -5,18 +8,26 @@ library("ggplot2")
 library(lubridate)
 library(scales)
 library(reconstructr)
+library("ggpubr")
 
 
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#IMPORT ALL THE DATA SETS
 query_data <- read_csv("query-data.2021.csv")
 industry_data <- read_csv("industry-data.csv")
 response_data <- read_csv("response-data.2021.csv")
+
 
 head(query_data)
 total_values<- nrow(query_data)
 
 total_missing_value_in_query<-sum(is.na(query_data$QUERY))
 
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #(Here if we see same queries can be seen in more than on session also
  #i.e query reformulation table row no. 89 and 105)
 session_query_counts <- query_data %>%  
@@ -30,41 +41,51 @@ repeated_query <- filtered_session_query_counts%>%
   group_by(QUERY)%>%
   summarise(Count = n())%>%arrange(desc(Count))
 
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #number queries in a session
 query_per_session <- query_data%>%
  group_by(JOB_ID)%>%
   summarise(COUNT = n())
- 
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 query_per_session1 <- query_data%>%
   group_by(JOB_ID)
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #how many queries per session in average
 mean_query_per_session <- mean(query_per_session$COUNT, na.rm = TRUE)
 max(query_per_session$COUNT)
 min(query_per_session$COUNT)
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #how the session last
 session_length_time <- query_data%>% group_by(JOB_ID)%>%
   mutate(session_duration = max(TIMESTAMP)-min(TIMESTAMP))%>%
   summarise(CATEGORIES,Duration = seconds_to_period(session_duration))
 
 
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #drop na containing row from session_length_time
 session_length_time_filtered <- na.omit(session_length_time)
 sum(is.na(session_length_time_filtered$CATEGORIES))
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #session length time grouped by categories and mean session length time produced
 session_length_by_categories <- session_length_time_filtered%>%group_by(CATEGORIES)%>%
   summarise(mean_duration = mean(Duration))
 
 
-
-
-
-
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #session length by industry
 session_length_by_industry <- inner_join(session_length_time_filtered,industry_data,by=c("JOB_ID" = "JOB_ID"))%>%
   group_by(INDUSTRY_SECTOR_NAME)%>%
@@ -80,10 +101,9 @@ ggplot(data=session_length_industry_summary,aes(x = mean_length, y= reorder(INDU
 
 
 
-
-
-
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 
 #what is the maximum session duration and which row
 max_session <- session_length_time_filtered%>%
@@ -96,6 +116,9 @@ max(session_length_time$session_duration) #who does this session length belongs 
 
 #df<-filter(industry_data, JOB_ID == 538240)
 
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #JOIN QUERY DATA & INDUSTRY DATA TO VISUALIZE PER industry_sector_name
 
 industry_mean_query_counts <- inner_join(query_data,industry_data,by=c("JOB_ID" = "JOB_ID")) %>%
@@ -110,19 +133,25 @@ ggplot(data=industry_mean_query_counts,aes(x = MEAN, y= reorder(INDUSTRY_SECTOR_
                   fill=MEAN))+geom_bar(stat = "identity")+
                   labs(x="MEAN", y="INDUSTRY_SECTOR_NAME", title = "MEAN Query Per Company")
 
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #most frequent queries per company (if compared above geombar is approved)
 frequent_queries_per_company <- industry_mean_query_counts%>%head(5)
 
 
-#distribution of queries by type
 
-  
-
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #Average queries per session grouped by Industry
 industy_query_count <- inner_join(industry_data,query_data, by="JOB_ID", "INDUSTRY_SECTOR_NAME")%>%
   group_by(INDUSTRY_SECTOR_NAME)%>%
   summarise(QUERY_COUNT = n())
 
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 #query success rate
 response_df <- response_data%>%
   mutate(success = ifelse(RESPONSE_TYPE ==1, "Recruiting","Matching"))
@@ -136,6 +165,9 @@ filter(response_df, JOB_ID == 153505)
   #mutate(rate = n/total)%>%
   #summarise(mean_rate = mean(rate))
 
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
 query_success <- inner_join(industry_data, response_df,by=c("JOB_ID" = "JOB_ID"))%>%
   group_by(INDUSTRY_SECTOR_NAME)%>%
   mutate(successful_responses = sum(RESPONSE_TYPE == 1), 
@@ -158,17 +190,28 @@ ggplot(data=industry_summary,aes(x = success_rate, y= reorder(INDUSTRY_SECTOR_NA
   
  
 
-#CORRELATION BETWEEN QUERY LENGTH AND SUCCESS RATE
+
+
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#CORRELATION BETWEEN LOCATION AND SUCCESS RATE
 correlation_query_success_location <- inner_join(query_per_session1, query_success1,by=c("JOB_ID" = "JOB_ID"))%>%
+  group_by(JOB_ID)%>%
   select(LOCATION_IDS, successful_responses)
 
 filtered_query_correlation_location <- na.omit(correlation_query_success_location)
 
-top_query_cor_location<-filtered_query_correlation_location%>%head(10)
+summarised_query_cor <- filtered_query_correlation_location%>%group_by(LOCATION_IDS)%>%
+  summarise(mean_rate = mean(successful_responses), n=n())
 
+#top_query_cor_location<-filtered_query_correlation_location%>%head(10)
+ggscatter(summarised_query_cor, x = "LOCATION_IDS", y = "successful_responses", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
   
 cor(top_query_cor_location$LOCATION_IDS, top_query_cor_location$successful_responses)
-
 
 
 plot(top_query_cor_location$LOCATION_IDS, top_query_cor_location$successful_responses)
@@ -189,3 +232,40 @@ merged_table <- merge(table1, table2, by = "id")
 
 # calculate the correlation between var1 and var2
 cor(merged_table$var1, merged_table$var2)
+
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#CORRELATION BETWEEN SALARY AND SUCCESS RATE
+correlation_query_success_salary <- inner_join(query_per_session1, query_success1,by=c("JOB_ID" = "JOB_ID"))%>%
+  group_by(JOB_ID)%>%
+  select(SALARY_MAX, successful_responses)
+
+filtered_query_correlation_salary <- na.omit(correlation_query_success_salary)
+
+summarised_query_cor_salary <- filtered_query_correlation_salary%>%group_by(SALARY_MAX)%>%
+  summarise(mean_success = mean(successful_responses), n=n())
+
+ggscatter(summarised_query_cor_salary, x = "SALARY_MAX", y = "mean_success", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "MAX SALARY", ylab = "MEAN_SUCCESS")
+
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#<-------------------------------------------------------->#
+#CORRELATION BETWEEN MINIMUM WORK EXPERIENCE AND SUCCESS RATE
+correlation_query_success_salary <- inner_join(query_per_session1, query_success1,by=c("JOB_ID" = "JOB_ID"))%>%
+  group_by(JOB_ID)%>%
+  select(SALARY_MAX, successful_responses)
+
+filtered_query_correlation_salary <- na.omit(correlation_query_success_salary)
+
+summarised_query_cor_salary <- filtered_query_correlation_salary%>%group_by(SALARY_MAX)%>%
+  summarise(mean_success = mean(successful_responses), n=n())
+
+ggscatter(summarised_query_cor_salary, x = "SALARY_MAX", y = "mean_success", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "MAX SALARY", ylab = "MEAN_SUCCESS")
+
